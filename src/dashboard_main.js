ca6e9d2529e74c6ed61eb92e25e9c71b95148ea1
Shane Corrie <134458972+Shaner1niner@users.noreply.github.com>
2026-05-19 12:00:16 +0000

    Load layer control patch before dashboard render

--- src/dashboard_main.js
+++ src/dashboard_main.js
@@ -1,6 +1,7 @@
 import { Store } from './Store.js';
 import { AssetPayloadLoader } from './AssetPayloadLoader.js?v=fix26_asset_loader_001';
 import { PlotlyRenderer } from './PlotlyRenderer.js?v=module_sentiment_price_alignment_hover_001';
+import './features/SentimentLayerStructureControlPatch.js?v=module_sentiment_layer_structure_controls_001';
 import { MarketTape } from './features/MarketTape.js';
 import './features/MarketTapeAttentionStructureCards.js?v=module_market_tape_attention_structure_cards_001';
 import './features/PublicDashboardIntroCopy.js?v=module_public_dashboard_intro_copy_001';
